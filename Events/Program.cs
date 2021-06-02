cc2c63e0ad6b8987e828234430b594d4769f79f7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 14:56:25 +0530

    Add files via upload

---
+++ Events/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Events
+{
+    public delegate void Sample(int x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int i = int.Parse(Console.ReadLine());
+            Sample s;                                   //instance
+            s = Square;                                 // point to the method
+            s.Invoke(i);                                //Invoking the delegate
+            Sample1 obj = new Sample1();
+            obj.Sample1Event += User1.User1Method;
+            obj.Sample1Event += User2.User2Method;
+            obj.Notify(i);
+        }
+        static void Square(int x)
+        {
+            Console.WriteLine(x * x);
+        }
+    }
+}
---
+++ Events/Sample1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Events
+{
+    class Sample1
+    {
+        public event Sample Sample1Event;
+        public void Notify(int x)
+        {
+            if (Sample1Event != null)
+            {
+                Sample1Event(x);
+            }
+        }
+    }
+    class User1
+    {
+        public static void User1Method(int x)
+        {
+            Console.WriteLine("Event received by User1 object");
+        }
+    }
+    class User2
+    {
+        public static void User2Method(int x)
+        {
+            Console.WriteLine("Event received by User2 object");
+        }
+    }
+}
