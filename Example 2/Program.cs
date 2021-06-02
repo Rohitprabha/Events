ae2be5fad2eb72f32acae71fd2155d1807d91b33
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 14:59:39 +0530

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Events_Example1
+{
+    public delegate void SampleDelegate();
+    class Program
+    {
+        public static event SampleDelegate Message;
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome Rohit");
+        }
+        public static void Greetings()
+        {
+            Message();
+        }
+        static void Main(string[] args)
+        {
+            Message += new SampleDelegate(Welcome);
+            Greetings();
+        }
+    }
+}
